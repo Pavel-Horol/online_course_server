e9e781343899a909ea363fb163a5174b08d9507d
Pasha Khorol <pashahorolskij4@gmail.com>
2024-10-24 13:04:13 +0300

    fix: fixed cookies for vercel

--- setup/cookie-setup.ts
+++ setup/cookie-setup.ts
@@ -2,5 +2,7 @@ import { CookieOptions } from "express";
 
 export const cookieConfig: CookieOptions = {
     maxAge: 7 * 24 * 60 * 60 * 1000,
-    httpOnly: true
+    httpOnly: true,
+    sameSite: 'none',
+    secure: true
 }
