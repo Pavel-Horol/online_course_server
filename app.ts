5f403608082953e16f748b451223c4b9ed9f5716
Pasha Khorol <pashahorolskij4@gmail.com>
2024-10-25 21:45:46 +0300

    fix: trying to fix

--- app.ts
+++ app.ts
@@ -20,7 +20,8 @@ app.use(cors({
     credentials: true,
     // origin: process.env.CLIENT_URL,
     origin: ['http://localhost:5173', 'https://online-course-client-inky.vercel.app'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE']
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
 }))
 app.use(helmet.contentSecurityPolicy({
     directives: {
